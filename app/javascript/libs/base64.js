e5eead77c8623a5cc7014de6b55bea97dd94f792
Jasper Lyons <jasper.lyons@gmail.com>
2020-02-24 14:46:59 +0000

    Remove negative lookbehind.
    Negative lookbehinds are only supported in Chrome, see:
    
    https://caniuse.com/#feat=js-regexp-lookbehind

--- app/javascript/libs/base64.js
+++ app/javascript/libs/base64.js
@@ -5,12 +5,16 @@ export const toBase64 = file =>
     reader.readAsDataURL(file);
     reader.onload = () => {
       const results = reader.result;
+      // Firefox doesn't support negative lookbehinds so instead of
+      // using a negative lookbehind to remove the required sections
+      // of the file, simply split at the look behind.
+      const [ result, content ] = results.split('base64,');
 
       return resolve({
-        result: results.replace(/^.*base64,/, ""),
+        result: result,
         fileType: file.type,
         fileName: file.name,
-        content: results.replace(/(?<=base64,)(.*)/, "")
+        content: content
       });
     };
     reader.onerror = error => reject(error);
