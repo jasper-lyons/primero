658b6b9b7e2d43058601254017458e0039271854
Jasper Lyons <jasper.lyons@gmail.com>
2020-11-17 10:21:50 +0000

    Fix timezone bug

--- app/javascript/components/key-performance-indicators/index.js
+++ app/javascript/components/key-performance-indicators/index.js
@@ -1,4 +1,4 @@
-export { default as DateRangeSelect } from "./components/date-range-select";
+export { DateRangeSelect } from "./components/date-range-select";
 export { default as TablePercentageBar } from "./components/table-percentage-bar";
 export { default as StackedPercentageBar } from "./components/stacked-percentage-bar";
 export { default as SingleAggregateMetric } from "./components/single-aggregate-metric";
